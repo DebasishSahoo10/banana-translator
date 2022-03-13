2e08c6ce0b92275b12ac4f9f1a5ad94f69a98900
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-03-14 00:54:05 +0530

    code : Linked API and HTML
    Linked HTML to JS and then JS to the API Server

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-var input = document.querySelector ("input-box")
-var translate = document.querySelector ("translate-btn")
-var output = document.querySelector ("output-box")
+var input = document.querySelector ("#input-box")
+var translateButton = document.querySelector ("#translate-btn")
+var output = document.querySelector ("#output-box")
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function apiURL (text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function clickHandler () {
+    var inputText = input.value
+    fetch(apiURL(inputText))
+    .then (response => response.json ())
+    .then (json => output.innerText = json.contents.translated)
+}
+
+translateButton.addEventListener ("click", clickHandler)
+
