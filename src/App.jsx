b1513d10732b098049afc68667e2f5de22187df8
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-11-17 21:19:24 +0530

    style : pushed wireframe deisgn

--- src/App.jsx
+++ src/App.jsx
@@ -19,18 +19,27 @@ function App() {
 
   return (
     <div className="App">
-<h1>Banana Translator</h1>
-      <input
-        placeholder="put your english text here"
-        onChange={userInput}
-      ></input>
-      <button onClick={fetchData}>Translate</button>
-      <div>{outputValue}</div>
-      <p>
-        Are you a fan of minions? Did you know that the gibberish they say is an
-        actual language. Use the translator to convert your text from English to
-        Minion speak or Banana language.
-      </p>
+    
+      <h1 className='tittle-box'>banana translator</h1>
+    
+      <div id='box'>
+        <input
+          placeholder="put your english text here"
+          onChange={userInput}
+          className='tittle-box'
+        ></input>
+        <button onClick={fetchData} className='tittle-box'>Translate</button>
+        <div className='tittle-box'>{outputValue}</div>
+        <p className='tittle-box'>
+          Are you a fan of minions? Did you know that the gibberish they say is an
+          actual language.
+        </p>
+        <p className='tittle-box'>
+          Use the translator to convert your text from English to
+          Minion speak or Banana language.
+        </p>
+      </div>
+      
     </div>
   )
 }
