3ebf22beed13b99afd82a824c18b936b716fd8eb
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-11-17 19:28:16 +0530

    feat : added base app code

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
 import { useState } from 'react'
 import './App.css'
+let serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [inputValue, setInput] = useState("");
+  let [outputValue, setOutput] = useState("");
+  let API = (text) => {
+    return serverURL + "?" + "text=" + text;
+  };
+  let fetchData = () => {
+    fetch(API(inputValue))
+      .then((response) => response.json())
+      .then((json) => setOutput(json.contents.translated));
+  };
+  let userInput = (e) => {
+    setInput(e.target.value);
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+<h1>Banana Translator</h1>
+      <input
+        placeholder="put your english text here"
+        onChange={userInput}
+      ></input>
+      <button onClick={fetchData}>Translate</button>
+      <div>{outputValue}</div>
+      <p>
+        Are you a fan of minions? Did you know that the gibberish they say is an
+        actual language. Use the translator to convert your text from English to
+        Minion speak or Banana language.
       </p>
     </div>
   )
